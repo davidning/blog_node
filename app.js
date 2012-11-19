37ffd3cd29ca19ae50e1f15015b31b9db0692ed6
rsilva <rsilva@rsilva.internal.hq.grupopie.com>
2012-11-19 14:37:44 +0000

    Config changes

--- app.js
+++ app.js
@@ -11,19 +11,19 @@ var express = require('express')
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3300);
-  app.set('views', __dirname + '/views');
+  app.set('port', process.env.PORT || config.port);
+  app.set('views', __dirname  + config.view_folder);
   app.set('view options', { pretty: true });
-  app.set('view engine', 'ejs');
+  app.set('view engine', config.view_engine);
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
-  app.use(express.cookieParser('your secret here'));
+  app.use(express.cookieParser(config.cookie));
   app.use(express.session());
   app.use(app.router);
-  app.use(require('stylus').middleware(__dirname + '/public'));
-  app.use(express.static(path.join(__dirname, 'public')));
+  app.use(require('stylus').middleware(__dirname + config.static_folder));
+  app.use(express.static(path.join(__dirname, config.static_folder)));
 });
 
 app.configure('development', function(){
